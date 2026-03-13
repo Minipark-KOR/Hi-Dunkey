3613f5e20e4537581d7f39ec07a47c6972642eb0
Minipark-KOR <minipark4u@gmail.com>
2026-03-13 20:32:56 +0900

    bug fix

--- core/base_collector.py
+++ core/base_collector.py
@@ -80,7 +80,7 @@ class BaseCollector:
 
         self.total_db_path = str(self.base_dir / f"{name}_total.db")
 
-        elf.debug_mode = kwargs.get('debug_mode', False)
+        self.debug_mode = kwargs.get('debug_mode', False)
 
         self.logger = build_logger(name, str(LOG_DIR / f"{name}.log"))
         if self.debug_mode:
